bdb8050f4828d9d618275fbe899a4539608e9245
Ehmad <justehmadsaeed@gmail.com>
2021-05-17 13:50:25 +0500

    📦 NEW: Pick and display image

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io' as io;
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_uploader/bottompicker_sheet.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,9 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Image Uploader',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
       ),
       home: HomePage(),
     );
@@ -23,22 +28,74 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final ImagePicker _picker = ImagePicker();
+  PickedFile _image;
+
+  _imageFromCamera() async {
+    final PickedFile image =
+        await _picker.getImage(source: ImageSource.camera, imageQuality: 50);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
+  _imageFromGallery() async {
+    final PickedFile image =
+        await _picker.getImage(source: ImageSource.gallery, imageQuality: 50);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Image Uploader'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              IconButton(icon: Icon(Icons.camera), onPressed: () {}),
-              IconButton(icon: Icon(Icons.upload_file), onPressed: () {}),
+              GestureDetector(
+                onTap: () {
+                  bottomPickerSheet(
+                      context, _imageFromCamera, _imageFromGallery);
+                },
+                child: CircleAvatar(
+                  radius: MediaQuery.of(context).size.width / 6,
+                  backgroundColor: Colors.grey,
+                  backgroundImage: _image != null
+                      ? FileImage(io.File(_image.path))
+                      : AssetImage('assets/camera_img.png'),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.file_upload),
+                      Text(
+                        'Upload Image',
+                      ),
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
-        ],
+        ),
       ),
     );
   }
